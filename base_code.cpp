28c3d40108a5f9314768f88d4e3ec01caf805249
huangtiehao <1634457608@qq.com>
2023-09-28 10:15:44 +0800

    chapter12finished

--- base_code.cpp
+++ base_code.cpp
@@ -1,7 +1,9 @@
 #define GLFW_INCLUDE_VULKAN
 
 #include <GLFW/glfw3.h>
+#include<algorithm>
 #include<iostream>
+#include<set>
 #include<stdexcept>
 #include<functional>
 #include<cstdlib>
@@ -12,6 +14,10 @@ const int HEIGHT = 600;
 const std::vector<const char*>validationLayers = { "VK_LAYER_KHRONOS_validation" };
 //是否开启校验层
 bool enableValidationLayers = true;
+
+const std::vector<const char*> deviceExtensions = {
+	VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
 
 VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
 	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
@@ -29,11 +35,18 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT
 		func(instance, debugMessenger, pAllocator);
 	}
 }
+struct SwapChainSupportDetails {
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR>formats;
+	std::vector<VkPresentModeKHR>presentModes;
+};
+
 struct QueueFamilyIndices {
-	int graphicsFamily=-1;
+	uint32_t graphicsFamily=-1;//支持绘制指令的队列族
+	uint32_t presentFamily = -1;//支持表现的队列族
 
 	bool isComplete() {
-		return graphicsFamily>=0;
+		return graphicsFamily>=0&&presentFamily>=0;
 	}
 };
 
@@ -47,11 +60,30 @@ public:
 		cleanup();
 	}
 private:
-	GLFWwindow* window;
-	VkInstance instance;
+	GLFWwindow* window;//GLFW窗口指针
+	VkInstance instance;//vulkan实例
+	VkSurfaceKHR surface;
+	//创建逻辑设备时指定的队列会随着逻辑设备一同被创建，在清除逻辑设备时，所拥有的队列也会
+	// 自动被清除，所以不用再cleanup函数中清除
+	VkQueue graphicsQueue;
+	VkQueue presentQueue;
+
+	VkPhysicalDevice physicalDevice= VK_NULL_HANDLE;//物理设备
+	VkDevice device;//虚拟设备
+
+	VkSwapchainKHR swapChain;
+	//交换链图像由交换链自己创建，自动清除，不需要我们手动创建和清除
+	//任何VkImage都需要绑定一个VkImageView对象来绑定他
+	std::vector<VkImage>swapChainImages;
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+	//VkImageView描述了图像访问的方式，以及图像的哪一部分可以被访问
+	std::vector<VkImageView>swapChainImageViews;
+
 	VkDebugUtilsMessengerEXT debugMessenger;
 	void initWindow()
 	{
+		glfwInit();
 		//显示地设置GLFW阻止它自动创建openGL上下文
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		//禁止窗口大小改变
@@ -66,11 +98,26 @@ private:
 	{
 		createinstance();
 		setupDebugMessenger();
+		//由于窗口表面对于物理设备的选择有一定影响，所以只能在vulkan实例创建之后
+		createSurface();
+		//获得物理设备
 		pickPhysicalDevice();
+		//逻辑设备并不直接与vulkan实例交互，所以创建逻辑设备时不需要使用vulkan实例作为参数
+		createLogicalDevice();
+		//创建交换链
+		createSwapChain();
+		createImageViews();
+		
+	}
+	void createSurface()
+	{
+		if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create window surface!");
+		}
 	}
 	void pickPhysicalDevice()
 	{
-		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 		uint32_t deviceCount = 0;
 		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
 		if (deviceCount == 0)
@@ -92,10 +139,182 @@ private:
 			throw std::runtime_error("failed to find a suitable GPU!");
 		}
 	}
+	void createImageViews()
+	{
+		swapChainImageViews.resize(swapChainImages.size());
+		size_t swapChainImages_size=swapChainImages.size();
+		for (size_t i = 0; i < swapChainImages_size; ++i)
+		{
+			VkImageViewCreateInfo createInfo = {};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = swapChainImages[i];
+			//指定一维纹理还是二维纹理还是三维纹理或立方体
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			//指定图象数据解析方式
+			createInfo.format = swapChainImageFormat;
+			//用于图像颜色通道的映射
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+			//用于指定图像的用途和哪一部分能够访问
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+			if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i])!=VK_SUCCESS)
+			{
+				throw std::runtime_error("failed to create ImageViews!");
+			}
+		}
+	}
+
+	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device)
+	{
+		SwapChainSupportDetails details;
+		vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+		uint32_t formatCount;
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+		if (formatCount != 0)
+		{
+			details.formats.resize(formatCount);
+			vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+		}
+		uint32_t presentModeCount;
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+		if (presentModeCount != 0)
+		{
+			details.presentModes.resize(presentModeCount);
+			vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+		}
+		return details;
+	}
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats)
+	{
+		if (availableFormats.size() == 1 && availableFormats[0].format == VK_FORMAT_UNDEFINED)
+		{
+			return { VK_FORMAT_B8G8R8A8_UNORM,VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
+		}
+		for (const auto& format : availableFormats)
+		{
+			if (format.format == VK_FORMAT_B8G8R8A8_UNORM 
+				&& format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR);
+			return format;
+		}
+		return availableFormats[0];
+	}
+	VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>availablePresentModes)
+	{
+		VkPresentModeKHR bestMode = VK_PRESENT_MODE_FIFO_KHR;
+		for (const auto& presentMode : availablePresentModes)
+		{
+			if (presentMode == VK_PRESENT_MODE_MAILBOX_KHR)
+			{
+				return presentMode;
+			}
+			else if (presentMode == VK_PRESENT_MODE_IMMEDIATE_KHR)
+			{
+				bestMode=presentMode;
+			}
+		}
+		return bestMode;
+	}
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device)
+	{
+		uint32_t extensionCount;
+		vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties>availableExtensions(extensionCount);
+		vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
+		std::set<std::string>requiredExtensions(deviceExtensions.begin(), deviceExtensions.end());
+		for (const auto& extension : availableExtensions)
+		{
+			requiredExtensions.erase(extension.extensionName);
+		}
+		return requiredExtensions.empty();
+	}
+
 	bool isDeviceSuitable(VkPhysicalDevice device) {
 		QueueFamilyIndices indices = findQueueFamilies(device);
+		bool extensionSupported = checkDeviceExtensionSupport(device);
+		bool swapChainAdequate = false;
+		if (extensionSupported)
+		{
+			SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
+			swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
+		}
+		return indices.isComplete() && extensionSupported&&swapChainAdequate;
+	}
+	//交换范围是交换链中的图像分辨率
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR&capabilities)
+	{
+		if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+		{
+			return capabilities.currentExtent;
+		}
+		else
+		{
+			VkExtent2D actualExtent = { WIDTH,HEIGHT };
+			actualExtent.width = std::min(capabilities.minImageExtent.width, std::max(capabilities.maxImageExtent.width, actualExtent.width));
+			actualExtent.height = std::min(capabilities.minImageExtent.height, std::max(capabilities.maxImageExtent.height, actualExtent.height));
+			return actualExtent;
+		}
 
-		return indices.isComplete();
+	}
+	void createSwapChain()
+	{
+		SwapChainSupportDetails swapChainSupport = querySwapChainSupport(physicalDevice);
+		VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
+		VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
+		VkExtent2D extent = chooseSwapExtent(swapChainSupport.capabilities);
+		//交换队列可以容纳的图像个数
+		uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+		if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount)
+		{
+			imageCount = swapChainSupport.capabilities.maxImageCount;
+		}
+		VkSwapchainCreateInfoKHR createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+		createInfo.surface = surface;
+		createInfo.minImageCount = imageCount;
+		createInfo.imageFormat = surfaceFormat.format;
+		createInfo.imageColorSpace = surfaceFormat.colorSpace;
+		createInfo.imageExtent = extent;
+		//每个图像所包含的层次
+		createInfo.imageArrayLayers = 1;
+		createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+		QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+		uint32_t queueFamilyIndices[] = { indices.graphicsFamily,indices.presentFamily };
+		if (indices.graphicsFamily != indices.presentFamily)
+		{
+			//图像可以在多个队列使用，不需要显式改变所有权，至少需要两个队列
+			createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+			createInfo.queueFamilyIndexCount = 2;
+			createInfo.pQueueFamilyIndices = queueFamilyIndices;
+		}
+		else
+		{
+			//一张图像一个时间只能在一个队列里，必须显式地改变所有权，性能最好
+			createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		}
+		//不进行固定的翻转操作
+		createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+		//忽略alpha通道
+		createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+		createInfo.presentMode = presentMode;
+		//设为true表示不关心被窗口系统中其他窗口遮挡的像素的颜色，这允许vulkan采取一定的优化
+		createInfo.clipped = VK_TRUE;
+		createInfo.oldSwapchain = VK_NULL_HANDLE;
+		VkSwapchainKHR swapChain;
+		if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS)
+		{
+			throw::std::runtime_error("failed to create swap chain!");
+		}
+		vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
+		swapChainImages.resize(imageCount);
+		vkGetSwapchainImagesKHR(device, swapChain, &imageCount, swapChainImages.data());
+		swapChainImageFormat = surfaceFormat.format;
+		swapChainExtent = extent;
 	}
 	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
 	{
@@ -109,9 +328,16 @@ private:
 		//找到一个支持VK_QUEUE__GRAPHICS_BIT的队列族
 		for (const auto& queueFamily : queueFamilies)
 		{
+
 			if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
 			{
 				indices.graphicsFamily = i;
+			}
+			VkBool32 presentSupport = false;
+			vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+			if (presentSupport)
+			{
+				indices.presentFamily = i;
 			}
 			if (indices.isComplete())
 			{
@@ -120,6 +346,50 @@ private:
 			i++;
 		}
 		return indices;
+	}
+	void createLogicalDevice()
+	{
+		QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+		std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+		std::set<uint32_t> uniqueQueueFamilies = { indices.graphicsFamily, indices.presentFamily };
+		for (uint32_t queueFamily : uniqueQueueFamilies)
+		{
+			VkDeviceQueueCreateInfo queueCreateInfo{};
+			queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+			queueCreateInfo.queueFamilyIndex = queueFamily;
+			queueCreateInfo.queueCount = 1;
+			//vulkan需要我们赋予一个0到1的浮点数来指定优先级，优先级可以控制指令的执行顺序
+			float queuePriority = 1.0;
+			queueCreateInfo.pQueuePriorities = &queuePriority;
+			queueCreateInfos.push_back(queueCreateInfo);
+		}
+
+		VkPhysicalDeviceFeatures deviceFeatures = {};
+		VkDeviceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		createInfo.pQueueCreateInfos = queueCreateInfos.data();
+		createInfo.queueCreateInfoCount = queueCreateInfos.size();
+		createInfo.pEnabledFeatures = &deviceFeatures;
+
+		createInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
+		createInfo.ppEnabledExtensionNames = deviceExtensions.data();
+		if (enableValidationLayers)
+		{
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
+		if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create logical device!");
+		}
+		//参数以此为逻辑设备队列族索引，队列索引，返回的队列句柄
+		vkGetDeviceQueue(device, indices.graphicsFamily, 0, &graphicsQueue);
+		vkGetDeviceQueue(device, indices.presentFamily,  0, &presentQueue);
 	}
 	void createinstance()
 	{
@@ -135,6 +405,7 @@ private:
 		appInfo.pApplicationName = "Hello,Triangle";
 		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion= VK_MAKE_VERSION(1, 0, 0);
 		appInfo.apiVersion = VK_API_VERSION_1_0;
 
 		//Vulkan倾向于通过结构体传递信息
@@ -155,6 +426,7 @@ private:
 		else
 		{
 			createInfo.enabledLayerCount = 0;
+			createInfo.pNext = nullptr;
 		}
 
 		//第一个参数是创建信息的结构体指针，第二个是自定义分配器回调函数，我们没有自定义分配器
@@ -194,6 +466,12 @@ private:
 	}
 	void cleanup()
 	{
+		for (auto& imageView : swapChainImageViews)
+		{
+			vkDestroyImageView(device, imageView, nullptr);
+		}
+		vkDestroySwapchainKHR(device, swapChain, nullptr);
+		vkDestroyDevice(device, nullptr);
 		if (enableValidationLayers) {
 			DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
 		}
@@ -252,7 +530,6 @@ private:
 };
 int main()
 {
-
 	HelloTriangleApplication app;
 	try {
 		app.run();
